30d2d7771947ef2e75139df31ccc4f83dd932d3e
asmaa <asmaaziry19@gmail.com>
2020-06-20 23:08:18 +0100

    backend

--- views.py
+++ views.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request,session ,redirect,url_for
+import plotly.graph_objects as go
 import requests
 import re
 import json
@@ -19,7 +20,7 @@ from testw import *
 from tweetEmbed import *
 from flask import Markup
 from localisation import *
-
+import connection_db
 app = Flask(__name__)
 
 
@@ -29,9 +30,9 @@ app = Flask(__name__)
 def index():
     # create sqlalchemy engine
     engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}"
-                        .format(user="root",
-                                pw="mercurycreatures12345",
-                                db="projet_dyal_pfe"))
+                        .format(user=connection_db.user,
+                                pw=connection_db.password,
+                                db=connection_db.database))
     
     #ar = request.form.getlist('airline')
     ar = request.args.get('airline')
@@ -86,10 +87,10 @@ def index():
 @app.route('/data')
 def data():
     try:
-        connection = mysql.connector.connect(host='localhost',
-                                            database='projet_dyal_pfe',
-                                            user='root',
-                                            password='mercurycreatures12345')
+        connection = mysql.connector.connect(host=connection_db.host,
+                                            database=connection_db.database,
+                                            user=connection_db.user,
+                                            password=connection_db.password)
 
         ##     Labels   ##
         sql_select_Query = "SELECT distinct airline from details order by airline"
@@ -103,6 +104,7 @@ def data():
         p= "positive"
         n ="negative"
         nt="neutral"
+        airlines=[]
         for row in records:
             wiki_details={}
             print(row[0])
@@ -111,39 +113,73 @@ def data():
             blabla.append(n)
             blabla.append(nt)
             wiki_details['airline_name']=row[0]
+            airlines.insert(0,row[0])
             wiki_details['summary']=wikipd(row[0])
             wiki_details['image'] = get_wiki_image(row[0])
             wiki.append(wiki_details)
             print(wiki)
             for sm in range(3) :
                 par.insert(0, row[0])
-        print(blabla)  
+        print(blabla)
+        
         ##    parents ##
         for sm in range(count) :
             par.insert(0, "")
         print(par)
         ##    values ##
-        val = [10, 2, 6, 6, 4, 4,4,5,6]
+        val = []
         sql_select_Tweets= "SELECT airline,count(airline) from details group by airline order by airline"
         cursor1 = connection.cursor()
         cursor1.execute(sql_select_Tweets)
         records1 = cursor1.fetchall()
         for row in records1:
             val.insert(0, row[1])
+        for airline in airlines:
+            sql_select_positive = "SELECT airline,count(sentiment) from details where sentiment='positive' and airline='%s'" %(airline)
+            sql_select_negative = "SELECT airline,count(sentiment) from details where sentiment='negative' and airline='%s'" %(airline)
+            sql_select_neutral = "SELECT airline,count(sentiment) from details where sentiment='neutral' and airline='%s'" %(airline)
+            cursorpos = connection.cursor()
+            cursorpos.execute(sql_select_positive)
+            cursorpos1 = cursorpos.fetchall()
+                
+            cursorneg = connection.cursor()
+            cursorneg.execute(sql_select_negative)
+            cursorneg1 = cursorneg.fetchall()
+                
+            cursornet = connection.cursor()
+            cursornet.execute(sql_select_neutral)
+            cursornet= cursornet.fetchall()
+            for row in cursorpos1:
+                val.append(row[1])
+            for row in cursorneg1:
+                val.append(row[1])
+            for row in cursornet:
+                val.append(row[1])
+              
         print(val)
         cursor1.close()
         sql_select_id_tweet = "SELECT id_tweet from details limit 25"
         cursor2 = connection.cursor()
         cursor2.execute(sql_select_id_tweet)
         records2 = cursor2.fetchall()
-        
+        aspects = ['Ground Service','Inflight Entertainement','Legroom', 'Value For Money', 'Food & Beverage', 'Cabin Staff Service']
+        vis=[]
+        for airline in airlines:
+            visual= []
+            for aspect in aspects:
+                sql_aspects = "SELECT count(aspect) from details where aspect='%s' and airline='%s' " %(aspect,airline)
+                cursor4 = connection.cursor()
+                cursor4.execute(sql_aspects)
+                records4 = cursor4.fetchall()
+                visual.append(records4[0][0])
+            vis.append(visual)
+            print(vis)
         data = []
         for tweet_id in records2 :
             tw = embedTw(tweet_id[0])
             data.append(Markup(tw))
-        
 
-
+     
     except Error as e:
         print("Error reading data from MySQL table", e)
     finally:
@@ -152,14 +188,14 @@ def data():
             cursor.close()
             print("MySQL connection is closed")
   
-    return render_template('kiki.html',dt= blabla,ff = par,values = val,wik =wiki,code = data )
+    return render_template('kiki.html',dt= blabla,ff = par,values = val,wik =wiki,code = data,air= airlines,vis=vis,aspects=aspects )
 @app.route('/mapl')
 def mapl():
     try:
-        connection = mysql.connector.connect(host='localhost',
-                                            database='projet_dyal_pfe',
-                                            user='root',
-                                            password='mercurycreatures12345')
+        connection = mysql.connector.connect(host=connection_db.host,
+                                            database=connection_db.database,
+                                            user=connection_db.user,
+                                            password=connection_db.password)
         cursor = connection.cursor()
         sql_select_location = "SELECT DISTINCT location FROM details WHERE location IS NOT NULL"
         cursor3 = connection.cursor()  
